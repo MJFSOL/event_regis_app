84b85c85fe07cbb57fbeacf129b7b563b5f46527
Mark <markflores082717@gmail.com>
2025-09-16 19:23:19 +0800

    updated

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -27,11 +27,15 @@ class AuthController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.role = "user"
+    @user.role = "admin"
 
     if @user.save
       session[:user_id] = @user.id
+      if @user.role == "admin"
+        redirect_to admin_dashboard_page_path, notice: "Account Created Successfully, Welcome Admin"
+      else
       redirect_to user_dashboard_page_path, notice: "Account Created Successfully, Welcome User"
+      end
     else
       render :signup, status: :unprocessable_entity, notice: "credential doesn't match"
     end
